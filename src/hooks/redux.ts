8b903de00ef36a72f469015790a8a23127004955
mpreter <dshabelyan@mail.ru>
2023-01-20 13:48:12 +0300

    Change config eslint

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -1,5 +1,5 @@
 import { useSelector, TypedUseSelectorHook, useDispatch } from 'react-redux';
 import { AppDispatch, RootState } from '../store';
 
-export const useAppDispatch: any = () => useDispatch<AppDispatch>();
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
 import { Provider } from 'react-redux';
+import App from './App';
 import { store } from './store/index';
 
 const root = ReactDOM.createRoot(
