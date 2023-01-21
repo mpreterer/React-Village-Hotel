ef28761fdb11139f6858245a03c9a8660419cc27
mpreter <dshabelyan@mail.ru>
2023-01-21 16:30:50 +0300

    distributed imports to groups

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import './index.scss';
+
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+
 import App from './App';
-import './index.scss';
-import { store } from './store/index';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
