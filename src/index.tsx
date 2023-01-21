a9368e396466b8ce00e5e896544f7a085ce932b8
mpreter <dshabelyan@mail.ru>
2023-01-21 10:06:00 +0300

    Sorted imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import { Provider } from 'react-redux';
 import App from './App';
+import './index.scss';
 import { store } from './store/index';
 
 const root = ReactDOM.createRoot(
