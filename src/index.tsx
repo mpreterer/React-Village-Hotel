72562129ef59b1bb5f999646d2f0a73215e4be43
mpreter <dshabelyan@mail.ru>
2023-01-21 17:25:24 +0300

    Change settings eslint for imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import './index.scss';
-
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from './App';
 import { store } from './store';
+
+import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
