d3b6e4e4a40350e5ae477ee37744e70223c8b343
CosmoboyMe <2014failz2014@gmail.com>
2023-01-23 13:29:10 +0100

    feat: create basis styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 
 import App from './App';
 import { store } from './store';
-import './index.scss';
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
