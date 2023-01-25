842749dfa12e697b92149405feac433d00f40cfb
Ekaterina <1988snea@gmail.com>
2023-01-25 19:34:48 +0500

    Заменила экспортируемые svg-файлы на material-icons issue #7

--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 
 import './Feature.scss';
+import 'material-icons/iconfont/material-icons.css';
 
 type TProps = {
   label: string;
@@ -11,9 +12,7 @@ type TProps = {
 const Feature: FC<TProps> = ({ label, description, imagePath }) => {
   return (
     <div className="feature">
-      <div className="feature__image-wrapper">
-        <img src={imagePath} alt={description} />
-      </div>
+      <span className="material-icons">{imagePath}</span>
       <span className="feature__name">{label}</span>
       <span className="feature__description">{description}</span>
     </div>
