9283a09d6aacfacd3712be65f19b7ccf2568f54a
CosmoboyMe <2014failz2014@gmail.com>
2023-01-26 09:56:32 +0100

    refactor: add FC type for Logo component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,13 +1,13 @@
+import { FC } from 'react';
+
 import logo from '../../assets/svg/logo.svg';
 
 import './Logo.scss';
 
-const Logo = () => {
-  return (
-    <div className="logo">
-      <img src={logo} alt="company logo" className="logo__img" />
-    </div>
-  );
-};
+const Logo: FC = () => (
+  <div className="logo">
+    <img src={logo} alt="company logo" className="logo__img" />
+  </div>
+);
 
 export { Logo };
