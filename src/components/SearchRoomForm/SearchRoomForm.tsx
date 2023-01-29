8c384be554d055c5db0955401a857bc94be474b0
CosmoboyMe <2014failz2014@gmail.com>
2023-01-29 14:29:56 +0100

    feat: create SearchRoomForm

---
+++ src/components/SearchRoomForm/SearchRoomForm.tsx
@@ -0,0 +1,40 @@
+import { FC, FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { SCREENS } from '../../routes/endpoints';
+import { DateDropdown } from '../DateDropdown/DateDropdown';
+import { Dropdown } from '../Dropdown/Dropdown';
+import { SubmitButton } from '../SubmitButton/SubmitButton';
+
+import { DROPDOWN_DECLENSIONS, DROPDOWN_ITEMS } from './constants';
+import './SearchRoomForm.scss';
+
+const SearchRoomForm: FC = () => {
+  const navigate = useNavigate();
+  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    navigate(SCREENS.SEARCH_ROOMS);
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit} className="search-rooms-form">
+      <h1 className="search-rooms-form__title">
+        Найдём номера под ваши пожелания
+      </h1>
+      <div className="search-rooms-form__calendar">
+        <DateDropdown hasTwoInputs />
+      </div>
+      <div className="search-rooms-form__dropdown">
+        <Dropdown
+          declensions={DROPDOWN_DECLENSIONS}
+          items={DROPDOWN_ITEMS}
+          dropdownType="guests"
+          placeholder="Сколько гостей"
+        />
+      </div>
+      <SubmitButton text="подобрать номер" />
+    </form>
+  );
+};
+
+export { SearchRoomForm };
