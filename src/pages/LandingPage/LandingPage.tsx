a31586ede88c4e32d577e99698daaeff5374f6f6
Ekaterina <1988snea@gmail.com>
2023-01-30 20:28:15 +0500

    feat: added Footer and SearchRoomForm to Landing Page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react';
 
+import { SearchRoomForm } from '../../components/SearchRoomForm/SearchRoomForm';
+
 import './LandingPage.scss';
 
 const LandingPage: FC = () => {
   return (
     <main className="landing-page">
       <div className="landing-page__wrapper">
-        <section className="landing-page__search-room-wrapper" />
+        <section className="landing-page__search-room-wrapper">
+          <SearchRoomForm />
+        </section>
         <span className="landing-page__text">
           Лучшие номера для вашей работы, отдыха и просто вдохновения
         </span>
