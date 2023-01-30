911087d21aef69d7abbd25a89ac5564121c0cb44
David <dshabelyan@mail.ru>
2023-01-30 13:33:03 +0300

    Added dropdown, dateDropdown,rangeSlider, CheckList, create constats

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,15 +1,65 @@
 import { FC } from 'react';
 
+import { CheckBox } from '../CheckBox/CheckBox';
+import { CheckList } from '../CheckList/CheckList';
+import { DateDropdown } from '../DateDropdown/DateDropdown';
+import { Dropdown } from '../Dropdown/Dropdown';
+import { RangeSlider } from '../RangeSlider/RangeSlider';
+
+import './Filters.scss';
+
+import {
+  CHECKBOXES,
+  DROPDOWN_DECLENSIONS,
+  DROPDOWN_ITEMS,
+  RANGE_SLIDER_RANGE,
+  RANGE_SLIDER_START,
+  RANGE_SLIDER_STEP,
+  RANGE_SLIDER_TITLE,
+} from './constants';
+
 const Filters: FC = () => {
   return (
     <aside className="filters">
-      <div className="filters__arrival-in-hotel" />
-      <div className="filters__guests-container" />
-      <div className="filters__price-hotel" />
-      <div className="filters__order-in-room" />
+      <div className="filters__arrival-in-hotel">
+        <DateDropdown />
+      </div>
+      <div className="filters__guests-container">
+        <Dropdown
+          declensions={DROPDOWN_DECLENSIONS}
+          items={DROPDOWN_ITEMS}
+          dropdownType="guests"
+          placeholder="Сколько гостей"
+        />
+      </div>
+      <div className="filters__price-hotel">
+        <RangeSlider
+          title={RANGE_SLIDER_TITLE}
+          start={RANGE_SLIDER_START}
+          step={RANGE_SLIDER_STEP}
+          range={RANGE_SLIDER_RANGE}
+        />
+      </div>
+      <div className="filters__order-in-room">
+        {CHECKBOXES.map((item) => (
+          <CheckBox
+            label={item.label}
+            name={item.name}
+            isRich={item.isRich}
+            key={item.name}
+          />
+        ))}
+      </div>
       <div className="filters__availability" />
       <div className="filters__furniture" />
-      <div className="filters__convenience" />
+      <div className="filters__convenience">
+        <CheckList
+          labelName="правила дома"
+          isToggleable
+          isRich={false}
+          listItems={[{ label: 'курение', name: 'Можно курить', id: 1 }]}
+        />
+      </div>
     </aside>
   );
 };
---
+++ src/components/Filters/constants.ts
@@ -0,0 +1,62 @@
+const DROPDOWN_DECLENSIONS = {
+  guests: ['гость', 'гостя', 'гостей'],
+  babies: ['младенец', 'младенца', 'младенцев'],
+};
+
+const DROPDOWN_ITEMS = [
+  { id: 'adults', name: 'Взрослые', amount: 2 },
+  { id: 'children', name: 'Дети', amount: 1 },
+  { id: 'babies', name: 'Младенцы', amount: 1 },
+];
+
+const RANGE_SLIDER_TITLE = 'Диапазон цены';
+const RANGE_SLIDER_START = [5000, 10000];
+const RANGE_SLIDER_STEP = 1;
+const RANGE_SLIDER_RANGE = { min: 0, max: 15000 };
+
+const CHECKBOXES = [
+  {
+    label: 'Можно курить',
+    name: 'Можно курить',
+    isRich: false,
+  },
+  {
+    label: 'Можно с питомцами',
+    name: 'Можно с питомцами',
+    isRich: false,
+  },
+  {
+    label: 'Можно пригласить гостей (до 10 человек)',
+    name: 'Можно пригласить гостей',
+    isRich: false,
+  },
+];
+
+const CHECKBOXES_RICH = [
+  {
+    label: 'Можно с питомцами',
+    name: 'Можно с питомцами',
+    isRich: true,
+  },
+  {
+    label: 'Можно с питомцами',
+    name: 'Можно с питомцами',
+    isRich: true,
+  },
+  {
+    label: 'Можно с питомцами',
+    name: 'Можно с питомцами',
+    isRich: true,
+  },
+];
+
+export {
+  CHECKBOXES,
+  CHECKBOXES_RICH,
+  DROPDOWN_DECLENSIONS,
+  DROPDOWN_ITEMS,
+  RANGE_SLIDER_RANGE,
+  RANGE_SLIDER_START,
+  RANGE_SLIDER_STEP,
+  RANGE_SLIDER_TITLE,
+};
