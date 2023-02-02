bd2f7cba3e0f9c8679f0bd8c44edfffc342201f4
David <dshabelyan@mail.ru>
2023-02-02 09:33:43 +0300

    Correct PP and vars

--- src/pages/SearchRooms/SearchRooms.tsx
+++ src/pages/SearchRooms/SearchRooms.tsx
@@ -1,20 +1,35 @@
+import { FC, useEffect, useState } from 'react';
+
 import { Filters } from '../../components/Filters/Filters';
 import { Pagination } from '../../components/Pagination/Pagination';
 
 import './SearchRooms.scss';
 
-const SearchRooms = () => {
+const SearchRooms: FC = () => {
+  // const [rooms, setRooms] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('https://react-village-d5bce-default-rtdb.firebaseio.com/')
+  //     .then((res) => res.json())
+  //     .then((arr) => {
+  //       setRooms(arr);
+  //     });
+  // }, []);
+
   return (
-    <div>
-      <div className="search-rooms">
+    <div className="search-rooms">
+      <div className="search-rooms__filters-container">
         <Filters />
       </div>
-      <div className="search-room__rooms-container">
-        <h2 className="search-room__number-for-user">
+      <div className="search-rooms__rooms-container">
+        <h2 className="search-rooms__title">
           Номера, которые мы для вас подобрали
         </h2>
-        <div className="search-room__rooms" />
-        <div className="search-room__pagination-container">
+        <div className="search-rooms__rooms">
+          {/* {rooms.map(() => (
+          ))} */}
+        </div>
+        <div className="search-rooms__pagination-container">
           <Pagination totalItems={180} itemsPerPage={12} />
         </div>
       </div>
