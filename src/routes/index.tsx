f0ba63657f816aa45d65149895de4d5f6f498142
Ekaterina <1988snea@gmail.com>
2023-02-04 01:02:45 +0500

    feat: page 404

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import { Layout } from '../components/Layout/Layout';
 import { LandingPage } from '../pages/LandingPage/LandingPage';
+import { NotFoundPage } from '../pages/NotFoundPage/NotFoundPage';
 import { SearchRooms } from '../pages/SearchRooms';
 
 import { SCREENS } from './endpoints';
@@ -12,6 +13,7 @@ const AppRoutes = () => {
       <Route path="/" element={<Layout />}>
         <Route path={SCREENS.LANDING} element={<LandingPage />} />
         <Route path={SCREENS.SEARCH_ROOMS} element={<SearchRooms />} />
+        <Route path={SCREENS.NOT_FOUND} element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
