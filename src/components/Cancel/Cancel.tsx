084edcdfa8aebc31d875f3ed2939f5e36e044b85
David <dshabelyan@mail.ru>
2023-02-06 21:34:17 +0300

    Correct semantic

--- src/components/Cancel/Cancel.tsx
+++ src/components/Cancel/Cancel.tsx
@@ -5,12 +5,12 @@ import './Cancel.scss';
 const Cancel: FC = () => {
   return (
     <div className="cancel">
-      <p className="cancel__title">Отмена</p>
-      <span className="cancel__text">
+      <h2 className="cancel__title">Отмена</h2>
+      <p className="cancel__text">
         Бесплатная отмена в течение 48 ч. После этого при отмене не позднее чем
         за 5 дн. до прибытия вы получите полный возврат за вычетом сбора за
         услуги.
-      </span>
+      </p>
     </div>
   );
 };
