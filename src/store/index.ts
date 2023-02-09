a852247187739c4b2748194d04414d191ec6304b
mpreter <dshabelyan@mail.ru>
2023-02-09 15:44:29 +0300

    added slice for get room info by id

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { authReducer } from './slices/auth/slice';
+import { roomInfoReducer } from './slices/room/slice';
 import { roomsReducer } from './slices/rooms/slice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     rooms: roomsReducer,
+    roomInfo: roomInfoReducer,
   },
 });
 
