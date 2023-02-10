a24490c3c18c3c4970e1904217449596b3861a14
Ekaterina <1988snea@gmail.com>
2023-02-10 17:58:15 +0500

    fix: change conditions for component Rooms rendering, move constant value to separate  file src\components\Rooms\constants.ts

--- src/components/Rooms/Rooms.tsx
+++ src/components/Rooms/Rooms.tsx
@@ -9,23 +9,30 @@ import {
 import { Pagination } from '../Pagination/Pagination';
 import { RoomCard } from '../RoomCard/RoomCard';
 
+import { ITEMS_PER_PAGE } from './constants';
 import './Rooms.scss';
 
 const Rooms: FC = () => {
-  const itemsPerPage = 12;
-
   const rooms = useSelector(roomsSelect);
   const currentPage = useSelector(activePageNumberSelect);
   const status = useSelector(statusSelect);
 
-  const indexFrom = (currentPage - 1) * itemsPerPage;
-  const indexTo = currentPage * itemsPerPage;
-
-  const isNoRooms =
-    (!rooms.length && status === 'resolved') || status === 'rejected';
+  const indexFrom = (currentPage - 1) * ITEMS_PER_PAGE;
+  const indexTo = currentPage * ITEMS_PER_PAGE;
 
   return (
     <div className="rooms">
+      {status === 'loading' && <div className="rooms__loader" />}
+      {status === 'rejected' && (
+        <div className="rooms__error-message">
+          произошла ошибка, повторите попытку позже
+        </div>
+      )}
+      {status === 'resolved' && !rooms.length && (
+        <div className="rooms__error-message">
+          нет комнат, соответствующих параметрам поиска
+        </div>
+      )}
       {!!rooms.length && (
         <>
           <div className="rooms__container">
@@ -44,17 +51,13 @@ const Rooms: FC = () => {
               );
             })}
           </div>
-          {!!(rooms.length > itemsPerPage) && (
+          {!!(rooms.length > ITEMS_PER_PAGE) && (
             <div className="rooms__pagination-container">
-              <Pagination itemsPerPage={itemsPerPage} />
+              <Pagination itemsPerPage={ITEMS_PER_PAGE} />
             </div>
           )}
         </>
       )}
-      {isNoRooms && (
-        <div className="rooms__error-message">комнаты не найдены</div>
-      )}
-      {status === 'loading' && <div className="rooms__loader" />}
     </div>
   );
 };
