4831eb6efb27e6dee0e483a6fb46528e79380e3f
Ekaterina <1988snea@gmail.com>
2023-02-10 17:30:03 +0500

    refactor: add axiosInstance, remove Firebase initialization

--- src/FirebaseAPI.ts
+++ src/FirebaseAPI.ts
@@ -2,36 +2,18 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import axios from 'axios';
-import { initializeApp } from 'firebase/app';
 
 import { RoomData } from './types/RoomData';
 
+const axiosInstance = axios.create({
+  baseURL: 'https://react-village-d5bce-default-rtdb.firebaseio.com/',
+});
+
 class FirebaseAPI {
-  constructor() {
-    FirebaseAPI.init();
-  }
-
-  static init() {
-    const firebaseConfig = {
-      apiKey: 'AIzaSyCzs3m1T-AwNOuezc9VVx8gWcrndQyIisY',
-      authDomain: 'react-village-d5bce.firebaseapp.com',
-      projectId: 'react-village-d5bce',
-      storageBucket: 'react-village-d5bce.appspot.com',
-      messagingSenderId: '903474401236',
-      appId: '1:903474401236:web:4e87d7adb9bc43c9361041',
-      measurementId: 'G-PHSNLX928V',
-    };
-
-    initializeApp(firebaseConfig);
-  }
-
   static async fetchRooms(rejectWithValue: (value: string) => any) {
     try {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const { data } = await axios.get(
-        'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json'
-      );
-      return data as RoomData[];
+      const { data } = await axiosInstance.get<RoomData[]>('rooms.json');
+      return data;
     } catch (error) {
       if (axios.isAxiosError(error)) {
         return rejectWithValue(error.message);
@@ -41,6 +23,4 @@ class FirebaseAPI {
   }
 }
 
-const FirebaseAPIInstance = new FirebaseAPI();
-
-export { FirebaseAPI, FirebaseAPIInstance };
+export { FirebaseAPI };
