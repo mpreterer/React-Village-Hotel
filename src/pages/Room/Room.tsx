bd30679d2a2e64371de33e17b293ebc71bb593a8
David <dshabelyan@mail.ru>
2023-02-10 14:59:55 +0300

    eslint:fix

--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -6,11 +6,11 @@ import classNames from 'classnames';
 import room1 from '../../assets/img/room-details/room-details-1.jpg';
 import room2 from '../../assets/img/room-details/room-details-2.jpg';
 import room3 from '../../assets/img/room-details/room-details-3.jpg';
+import { BookingForm } from '../../components/BookingForm/BookingForm';
 import { BulletList } from '../../components/BulletList/BulletList';
 import { useAppDispatch } from '../../hooks/redux';
 import { roomInfo } from '../../store/slices/room/selectors';
 import { fetchRoomInfoById } from '../../store/slices/room/slice';
-import { BookingForm } from '../../components/BookingForm/BookingForm';
 
 import { convertRules } from './helpers';
 import './Room.scss';
