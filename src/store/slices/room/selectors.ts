39467ec489712dfd0e12fd3b80ceabe3e02d0b2a
mpreter <dshabelyan@mail.ru>
2023-02-13 21:53:44 +0300

    Added selector for status request

--- src/store/slices/room/selectors.ts
+++ src/store/slices/room/selectors.ts
@@ -1,5 +1,6 @@
 import { RootState } from '../../index';
 
 const room = (state: RootState) => state.room.room;
+const statusRequest = (state: RootState) => state.room.status;
 
-export { room };
+export { room, statusRequest };
