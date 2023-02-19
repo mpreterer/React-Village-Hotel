15d55d6470be6e83463564e3b0883878a637db03
CosmoboyMe <2014failz2014@gmail.com>
2023-02-19 19:30:05 +0100

    feat: create enums for SignUpForm

---
+++ src/components/SignUpForm/constants.ts
@@ -0,0 +1,16 @@
+const enum SignUpFormNames {
+  Name = 'name',
+  Surname = 'surname',
+  Gender = 'gender',
+  Birthdate = 'birthdate',
+  Email = 'email',
+  Password = 'password',
+  Special = 'special',
+}
+
+const enum Genders {
+  Man = 'man',
+  Woman = 'woman',
+}
+
+export { Genders, SignUpFormNames };
