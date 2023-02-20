0b33ed73f7bd2fd4f78aff8c3db9c6a6cf8fa80b
anvyden <anvyden@gmail.com>
2023-02-20 21:03:36 +0700

    feat: create signUp thunk and add cases for that. Create helpers and authError types

--- src/store/slices/auth/slice.ts
+++ src/store/slices/auth/slice.ts
@@ -1,19 +1,100 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios, { AxiosError } from 'axios';
+
+import { FirebaseAPI } from '../../../FirebaseAPI';
+import { SignUpData } from '../../../types/AuthData';
+
+import { AuthError, updateLocalStorage, UserData } from './helpers';
 
 type InitialState = {
   isAuth: boolean;
-  userName: null | string;
+  token: string | null;
+  refreshToken: string | null;
+  expirationTime: string | null;
+  userId: string | null;
+  userName: string | null;
+  userSurname: string | null;
+  error: AuthError | string | null;
+  status: string;
 };
 
 const initialState: InitialState = {
-  isAuth: false,
-  userName: null,
+  isAuth: !!localStorage.getItem('token'),
+  token: localStorage.getItem('token') || null,
+  refreshToken: localStorage.getItem('refreshToken') || null,
+  expirationTime: localStorage.getItem('expirationTime') || null,
+  userId: localStorage.getItem('userId') || null,
+  userName: localStorage.getItem('userName') || null,
+  userSurname: localStorage.getItem('userSurname') || null,
+  error: null,
+  status: 'idle',
 };
+
+const NAMESPACE = 'auth';
+
+export const signUp = createAsyncThunk<
+  UserData,
+  SignUpData,
+  { rejectValue: AxiosError<{ error: AuthError }> | string }
+>(`${NAMESPACE}/signUp`, async (signUpData, { rejectWithValue }) => {
+  try {
+    const { data } = await FirebaseAPI.singUp(signUpData);
+
+    const fullName = data.displayName.split(' ');
+
+    const expirationTime = new Date(
+      new Date().getTime() + Number(data.expiresIn) * 1000
+    ).toISOString();
+
+    const userData = {
+      token: data.idToken,
+      refreshToken: data.refreshToken,
+      expirationTime,
+      userId: data.localId,
+      userName: fullName[0],
+      userSurname: fullName[1],
+    };
+
+    return userData;
+  } catch (error) {
+    return rejectWithValue(
+      axios.isAxiosError(error) ? error : 'An unexpected error occurred'
+    );
+  }
+});
 
 const slice = createSlice({
   name: 'auth',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(signUp.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(signUp.fulfilled, (state, { payload }) => {
+        updateLocalStorage('set', payload);
+
+        return {
+          ...state,
+          ...payload,
+          status: 'resolved',
+          isAuth: !!payload.token,
+        };
+      })
+      .addCase(signUp.rejected, (state, { payload }) => {
+        state.status = 'rejected';
+
+        if (payload instanceof AxiosError && payload.response) {
+          state.error = payload.response?.data.error;
+        }
+
+        if (typeof payload === 'string') {
+          state.error = payload;
+        }
+      });
+  },
 });
 
 export const authReducer = slice.reducer;
