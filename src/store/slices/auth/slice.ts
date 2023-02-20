a9abd1b20692cf544a8a3a349aa6699c210b8f51
anvyden <anvyden@gmail.com>
2023-02-20 21:05:55 +0700

    feat: create signIn thunk and add cases for that

--- src/store/slices/auth/slice.ts
+++ src/store/slices/auth/slice.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios, { AxiosError } from 'axios';
 
 import { FirebaseAPI } from '../../../FirebaseAPI';
-import { SignUpData } from '../../../types/AuthData';
+import { SignInData, SignUpData } from '../../../types/AuthData';
 
 import { AuthError, updateLocalStorage, UserData } from './helpers';
 
@@ -63,6 +63,38 @@ export const signUp = createAsyncThunk<
   }
 });
 
+export const signIn = createAsyncThunk<
+  UserData,
+  Omit<SignInData, 'returnSecureToken'>,
+  { rejectValue: AxiosError<{ error: AuthError }> | string }
+>(`${NAMESPACE}/signIn`, async (signInData, { rejectWithValue }) => {
+  try {
+    const { data } = await FirebaseAPI.signIn(signInData);
+
+    const fullName = data.displayName.split(' ');
+
+    const expirationTime = new Date(
+      new Date().getTime() + Number(data.expiresIn) * 1000
+    ).toISOString();
+
+    const userData = {
+      token: data.idToken,
+      refreshToken: data.refreshToken,
+      expirationTime,
+      userId: data.localId,
+      userName: fullName[0],
+      userSurname: fullName[1],
+    };
+
+    return userData;
+  } catch (error) {
+    console.log(error);
+    return rejectWithValue(
+      axios.isAxiosError(error) ? error : 'An unexpected error occurred'
+    );
+  }
+});
+
 const slice = createSlice({
   name: 'auth',
   initialState,
@@ -84,6 +116,32 @@ const slice = createSlice({
         };
       })
       .addCase(signUp.rejected, (state, { payload }) => {
+        state.status = 'rejected';
+
+        if (payload instanceof AxiosError && payload.response) {
+          state.error = payload.response?.data.error;
+        }
+
+        if (typeof payload === 'string') {
+          state.error = payload;
+        }
+      })
+
+      .addCase(signIn.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(signIn.fulfilled, (state, { payload }) => {
+        updateLocalStorage('set', payload);
+
+        return {
+          ...state,
+          ...payload,
+          status: 'resolved',
+          isAuth: !!payload.token,
+        };
+      })
+      .addCase(signIn.rejected, (state, { payload }) => {
         state.status = 'rejected';
 
         if (payload instanceof AxiosError && payload.response) {
