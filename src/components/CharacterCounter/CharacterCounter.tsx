41a0d6e2a448bba206f2be827a6a2864b4aac142
CosmoboyMe <2014failz2014@gmail.com>
2023-02-21 03:07:31 +0100

    feat: create CharacterCounter

---
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -0,0 +1,64 @@
+import { FC } from 'react';
+
+import { RADIUS } from './constants';
+import './CharacterCounter.scss';
+
+type Props = {
+  maxCount: number;
+  currentCount: number;
+};
+
+const CharacterCounter: FC<Props> = ({ maxCount, currentCount }) => {
+  const value = currentCount > maxCount ? maxCount : currentCount;
+
+  const percent = (value / maxCount) * 100;
+
+  let strokeColor = '#BC9CFF';
+  if (percent >= 50) {
+    strokeColor = '#6FCF97';
+  }
+  if (percent >= 97) {
+    strokeColor = '#BD2323';
+  }
+
+  const strokeDashoffset = 2 * Math.PI * RADIUS;
+  const offset = strokeDashoffset - (percent / 100) * strokeDashoffset;
+
+  return (
+    <div className="character-counter">
+      <svg className="character-counter__svg" viewBox="0 0 44 44">
+        <circle
+          fill="none"
+          cx="22"
+          cy="22"
+          r={RADIUS}
+          stroke="#969494"
+          strokeWidth={4}
+        />
+        <circle
+          className="character-counter__progress"
+          cx="-22"
+          cy="22"
+          r={RADIUS}
+          fill="none"
+          stroke={strokeColor}
+          strokeDasharray={`${strokeDashoffset} ${strokeDashoffset}`}
+          strokeDashoffset={offset}
+          style={{ transform: 'rotate(-90deg)' }}
+          strokeWidth={4}
+        />
+        <text
+          textAnchor="middle"
+          x="22"
+          y="26"
+          fill={strokeColor}
+          className="character-counter__text"
+        >
+          {value || ''}
+        </text>
+      </svg>
+    </div>
+  );
+};
+
+export { CharacterCounter };
