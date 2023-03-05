3e218b84e2ef766ae6f81828777a565ed796351c
David <dshabelyan@mail.ru>
2023-03-05 21:51:11 +0300

    added check status and new props for room booking card

--- src/components/BookingRooms/BookingRooms.tsx
+++ src/components/BookingRooms/BookingRooms.tsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { ITEMS_PER_PAGE } from '../../shared/constants/paginationItems';
 import {
+  errorMessageSelect,
   profileSelect,
   statusSelect,
 } from '../../store/slices/profile/selectors';
@@ -20,6 +21,7 @@ const BookingRooms: FC = () => {
   const currentPage = useSelector(activePageNumberSelect);
   const bookedRooms = useAppSelector(profileSelect);
   const status = useAppSelector(statusSelect);
+  const errorMessage = useAppSelector(errorMessageSelect);
 
   const indexFrom = (currentPage - 1) * ITEMS_PER_PAGE;
   const indexTo = currentPage * ITEMS_PER_PAGE;
@@ -35,41 +37,55 @@ const BookingRooms: FC = () => {
           <Loader />
         </div>
       )}
-      {status === 'rejected' && !bookedRooms && (
+      {status === 'rejected' && bookedRooms === null && (
         <div className="booking-rooms__error-message">
-          Бронирования не найдены
+          У вас нет бронирований
         </div>
       )}
-      {status === 'resolved' && bookedRooms && (
-        <>
-          <div className="booking-rooms__container">
-            {bookedRooms.slice(indexFrom, indexTo).map((room, index) => (
-              <RoomBookingCard
-                key={String(room.bookingId)}
-                id={String(room.roomNumber)}
-                roomNumber={room.roomNumber}
-                price={room.price}
-                reviewsCount={room.reviewsCount}
-                rateNumber={room.rating}
-                imgsSrc={room.images}
-                totalAmount={room.totalAmount}
-                bookingStatus={room.bookingStatus}
-                bookingId={String(room.bookingId)}
-                isLux={room.isLux}
-              />
-            ))}
+      {status === 'resolved' &&
+        bookedRooms !== null &&
+        bookedRooms.length === 0 && (
+          <div className="booking-rooms__error-message">
+            У вас нет бронирований
           </div>
-          {bookedRooms.length > ITEMS_PER_PAGE && (
-            <div className="booking-rooms__pagination-container">
-              <Pagination
-                totalRooms={180}
-                itemsPerPage={ITEMS_PER_PAGE}
-                onClickPage={handlePaginationPageClick}
-              />
-            </div>
-          )}
-        </>
+        )}
+      {status === 'rejected' && errorMessage !== 'Bookings not found' && (
+        <div className="booking-rooms__error-message">
+          Произошла ошибка, повторите позже
+        </div>
       )}
+      {status === 'resolved' &&
+        bookedRooms !== null &&
+        bookedRooms?.length > 0 && (
+          <>
+            <div className="booking-rooms__container">
+              {bookedRooms.slice(indexFrom, indexTo).map((room) => (
+                <RoomBookingCard
+                  key={String(room.bookingId)}
+                  id={String(room.roomNumber)}
+                  roomNumber={room.roomNumber}
+                  price={room.price}
+                  reviewsCount={room.reviewsCount}
+                  rateNumber={room.rating}
+                  imgsSrc={room.images}
+                  totalAmount={room.totalAmount}
+                  bookingStatus={room.bookingStatus}
+                  bookingId={String(room.bookingId)}
+                  isLux={room.isLux}
+                />
+              ))}
+            </div>
+            {bookedRooms.length > ITEMS_PER_PAGE && (
+              <div className="booking-rooms__pagination-container">
+                <Pagination
+                  totalRooms={180}
+                  itemsPerPage={ITEMS_PER_PAGE}
+                  onClickPage={handlePaginationPageClick}
+                />
+              </div>
+            )}
+          </>
+        )}
     </div>
   );
 };
