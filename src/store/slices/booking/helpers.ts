d8a3ddf0cef0924a8eefa45845233454e5b9bc2d
Ekaterina <1988snea@gmail.com>
2023-03-05 22:44:17 +0500

    refactor: use dot as a  delimiter in getDateFromString helper

--- src/store/slices/booking/helpers.ts
+++ src/store/slices/booking/helpers.ts
@@ -1,5 +1,5 @@
 const getDateFromString = (dateString: string) =>
-  new Date(dateString.split('.').reverse().join());
+  new Date(dateString.split('.').reverse().join('.'));
 
 const sortDates = (datesArray: [from: Date, to: Date][]) =>
   datesArray.sort((a, b) => (a[0] > b[0] ? 1 : -1));
