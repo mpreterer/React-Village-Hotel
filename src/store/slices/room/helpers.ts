b4216586c12089ef991c352dc89caa70f3fc2e9c
Ekaterina <1988snea@gmail.com>
2023-03-09 21:07:05 +0500

    refactor: remove unused helper file

--- src/store/slices/room/helpers.ts
+++
@@ -1,9 +0,0 @@
-import { AsyncThunk } from '@reduxjs/toolkit';
-
-import { RoomData } from '../../../types/RoomData';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type GenericAsyncThunk = AsyncThunk<RoomData, unknown, any>;
-type FulfilledAction = ReturnType<GenericAsyncThunk['fulfilled']>;
-
-export type { FulfilledAction };
