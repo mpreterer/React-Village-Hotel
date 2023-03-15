071167346e314ddb6e65ff2b1111fb561d29ded1
mpreter <dshabelyan@mail.ru>
2023-03-15 16:58:18 +0300

    removed constant guests and correct set values in Landing from filters slice

--- src/components/SearchRoomForm/SearchRoomForm.tsx
+++ src/components/SearchRoomForm/SearchRoomForm.tsx
@@ -1,18 +1,20 @@
 import { FC, FormEvent, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { useAppDispatch } from '../../hooks/redux';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { SCREENS } from '../../routes/endpoints';
+import { filterSelect } from '../../store/slices/filters/selectors';
 import { filtersActions } from '../../store/slices/filters/slice';
 import { DropdownGuestsItemData } from '../../types/DropdownItemData';
 import { DateDropdown } from '../DateDropdown/DateDropdown';
 import { DropdownGuests } from '../DropdownGuests/DropdownGuests';
 import { SubmitButton } from '../SubmitButton/SubmitButton';
 
-import { DROPDOWN_ITEMS } from './constants';
 import './SearchRoomForm.scss';
 
 const SearchRoomForm: FC = () => {
+  const filters = useAppSelector(filterSelect);
+  const { capacity, selectedDates } = filters;
   const navigate = useNavigate();
   const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -37,12 +39,18 @@ const SearchRoomForm: FC = () => {
         Найдём номера под ваши пожелания
       </h1>
       <div className="search-rooms-form__calendar">
-        <DateDropdown hasTwoInputs onSelect={handleDateDropdownSelect} />
+        <DateDropdown
+          hasTwoInputs
+          onSelect={handleDateDropdownSelect}
+          initialDates={selectedDates}
+        />
       </div>
       <div className="search-rooms-form__dropdown">
         <DropdownGuests
-          items={DROPDOWN_ITEMS}
+          items={capacity.items}
           onChange={handleGuestDropdownChange}
+          guestsLimit={capacity.guestsLimit}
+          babiesLimit={capacity.babiesLimit}
         />
       </div>
       <SubmitButton text="подобрать номер" />
