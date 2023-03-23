4b803aabc6189c639f3e44601cc82bb21253d64c
Ekaterina <1988snea@gmail.com>
2023-03-23 19:23:55 +0500

    testing: check routing to all pages

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -6,9 +6,12 @@ import '@testing-library/jest-dom';
 import { App } from '../App';
 import { mockedStore } from '../shared/testUtils/mockedStore';
 import { renderWithProviders } from '../shared/testUtils/testUtils';
+import { initialState as authInitialState } from '../store/slices/auth/slice';
+import { initialState as roomsInitialState } from '../store/slices/rooms/slice';
 
 describe('Application rendering', () => {
-  it('Renders application with correct routing', async () => {
+  it(`Renders application with correct routing 
+  when user is not authorized`, async () => {
     renderWithProviders(
       <BrowserRouter>
         <App />
@@ -18,21 +21,80 @@ describe('Application rendering', () => {
       }
     );
 
-    const searchRoomsLink = screen.getByText(/подобрать номер/);
-    expect(searchRoomsLink).toBeInTheDocument();
-    fireEvent.click(searchRoomsLink);
+    fireEvent.click(screen.getByText(/подобрать номер/));
     expect(await screen.findByTitle(/ожидание загрузки/i)).toBeInTheDocument();
 
-    const aboutLink = screen.getByText(/о нас/);
-    expect(aboutLink).toBeInTheDocument();
-    fireEvent.click(aboutLink);
+    fireEvent.click(screen.getByText(/о нас/));
     expect(
       await screen.findByText(/Найдём номера под ваши пожелания/i)
     ).toBeInTheDocument();
 
-    const serviceLink = screen.getByText(/услуги/);
-    expect(serviceLink).toBeInTheDocument();
-    fireEvent.click(serviceLink);
+    fireEvent.click(screen.getByText(/услуги/));
     expect(await screen.findByText(/404/i)).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText(/войти/));
+    expect(await screen.findByText('Войти')).toBeInTheDocument();
+    expect(
+      await screen.findByText('Нет аккаунта на Toxin?')
+    ).toBeInTheDocument();
+    expect(await screen.findByText('создать')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText(/зарегистрироваться/));
+    expect(await screen.findByText('Регистрация аккаунта')).toBeInTheDocument();
+  });
+
+  it(`Renders application with correct routing 
+  when user is authorized`, async () => {
+    const room = {
+      roomNumber: 111,
+      furniture: [
+        { id: 'bedrooms', limit: 1 },
+        { id: 'beds', limit: 2 },
+        { id: 'bathrooms', limit: 2 },
+      ],
+      capacity: [{ id: 'guest', limit: 2 }],
+      reservedDates: [{ from: '', to: '' }],
+      details: {},
+      images: [''],
+      imagesDetailed: [''],
+      isLux: false,
+      price: 5500,
+      rating: 4,
+      feedbackCount: 2,
+      information: {},
+    };
+    const app = renderWithProviders(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+      {
+        preloadedState: {
+          ...mockedStore,
+          auth: {
+            ...authInitialState,
+            isAuth: true,
+            userName: 'TestName',
+            userSurname: 'TestSurname',
+          },
+          rooms: {
+            ...roomsInitialState,
+            rooms: [room],
+            errorMessage: null,
+            status: 'resolved',
+          },
+        },
+      }
+    );
+
+    fireEvent.pointerDown(screen.getByText('TestName TestSurname'));
+    expect(
+      await screen.findByText('Забронированные номера')
+    ).toBeInTheDocument();
+    fireEvent.click(screen.getByText(/о нас/));
+    fireEvent.click(screen.getByText(/подобрать номер/));
+    expect(await screen.findByText('111')).toBeInTheDocument();
+    fireEvent.click(await screen.findByText('111'));
+
+    expect(app).toMatchSnapshot();
   });
 });
