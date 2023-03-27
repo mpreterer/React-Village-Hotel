5c1a44ad81789e35aba3debb0da390ef0842f056
Ekaterina <1988snea@gmail.com>
2023-03-27 20:39:50 +0500

    testing: fix mocks, add more cases for booking

--- src/store/slices/booking/__tests__/slice.test.ts
+++ src/store/slices/booking/__tests__/slice.test.ts
@@ -8,9 +8,9 @@ import { makeBooking as makeBookingThunk } from '../slice';
 
 const server = setupServer(
   rest.post(
-    'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms/0/bookedDates.json',
+    'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms/1/bookedDates.json',
     (req, res, ctx) => {
-      return res(ctx.status(200));
+      return res(ctx.status(200), ctx.json({ bookingID: '123' }));
     }
   ),
 
@@ -50,7 +50,7 @@ const bookingData = {
 };
 
 describe('Booking', () => {
-  it('successfull booking', async () => {
+  it('successful booking - first booking of the room', async () => {
     const thunk = makeBookingThunk({
       ...bookingData,
       userId: 'testUser',
@@ -68,68 +68,260 @@ describe('Booking', () => {
     expect(end[0].payload).toHaveProperty('bookingId');
   });
 
-  // it('booking failure', async () => {
-  //   server.use(
-  //     rest.get(
-  //       'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
-  //       (req, res, ctx) => {
-  //         return res(ctx.status(404));
-  //       }
-  //     )
-  //   );
-  //   const thunk = makeBookingThunk({
-  //     ...bookingData,
-  //     userId: 'testUser',
-  //   });
-  //   await thunk(
-  //     dispatch,
-  //     () => {},
-  //     () => {}
-  //   );
-  //   const [start, end] = dispatch.mock.calls;
-  //   expect(start[0].type).toBe('booking/makeBooking/pending');
-  //   expect(start[0].payload).toBe(undefined);
-  //   expect(end[0].type).toBe('booking/makeBooking/rejected');
-  //   expect(end[0].payload).toBe('Request failed with status code 404');
-  // });
+  it('successful booking - booking before existing bookings', async () => {
+    server.use(
+      rest.get(
+        'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
+        (req, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({
+              1: {
+                bookedDates: {
+                  bookingID1: {
+                    dates: { from: '03.04.2023', to: '06.04.2023' },
+                    userId: 'testUser',
+                  },
 
-  // it('room already booked', async () => {
-  //   server.use(
-  //     rest.get(
-  //       'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
-  //       (req, res, ctx) => {
-  //         return res(
-  //           ctx.status(200),
-  //           ctx.json({
-  //             bookedDates: {
-  //               bookedDates: {
-  //                 bookingID: {
-  //                   dates: { from: '01.04.2023', to: '03.04.2023' },
-  //                   userId: 'testUser',
-  //                 },
-  //               },
-  //             },
-  //             roomNumber: 1,
-  //           })
-  //         );
-  //       }
-  //     )
-  //   );
-  //   const thunk = makeBookingThunk({
-  //     ...bookingData,
-  //     userId: 'testUser',
-  //   });
-  //   await thunk(
-  //     dispatch,
-  //     () => {},
-  //     () => {}
-  //   );
-  //   const [start, end] = dispatch.mock.calls;
-  //   expect(start[0].type).toBe('booking/makeBooking/pending');
-  //   expect(start[0].payload).toBe(undefined);
-  //   expect(end[0].type).toBe('booking/makeBooking/rejected');
-  //   expect(end[0].payload).toBe(
-  //     'На данный период проживания комната уже забронирована'
-  //   );
-  // });
+                  bookingID2: {
+                    dates: { from: '14.04.2023', to: '16.04.2023' },
+                    userId: 'testUser',
+                  },
+                },
+              },
+            })
+          );
+        }
+      )
+    );
+    const thunk = makeBookingThunk({
+      ...bookingData,
+      userId: 'testUser',
+    });
+    await thunk(
+      dispatch,
+      () => {},
+      () => {}
+    );
+    const [start, end] = dispatch.mock.calls;
+    expect(start[0].type).toBe('booking/makeBooking/pending');
+    expect(start[0].payload).toBe(undefined);
+    expect(end[0].type).toBe('booking/makeBooking/fulfilled');
+    expect(end[0].payload).toMatchObject(bookingData);
+    expect(end[0].payload).toHaveProperty('bookingId');
+  });
+
+  it('successful booking - booking after existing bookings', async () => {
+    server.use(
+      rest.get(
+        'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
+        (req, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({
+              1: {
+                bookedDates: {
+                  bookingID1: {
+                    dates: { from: '04.03.2023', to: '06.03.2023' },
+                    userId: 'testUser',
+                  },
+
+                  bookingID2: {
+                    dates: { from: '30.03.2023', to: '01.04.2023' },
+                    userId: 'testUser',
+                  },
+                },
+              },
+            })
+          );
+        }
+      )
+    );
+    const thunk = makeBookingThunk({
+      ...bookingData,
+      userId: 'testUser',
+    });
+    await thunk(
+      dispatch,
+      () => {},
+      () => {}
+    );
+    const [start, end] = dispatch.mock.calls;
+    expect(start[0].type).toBe('booking/makeBooking/pending');
+    expect(start[0].payload).toBe(undefined);
+    expect(end[0].type).toBe('booking/makeBooking/fulfilled');
+    expect(end[0].payload).toMatchObject(bookingData);
+    expect(end[0].payload).toHaveProperty('bookingId');
+  });
+
+  it('successful booking - booking between existing bookings', async () => {
+    server.use(
+      rest.get(
+        'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
+        (req, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({
+              1: {
+                bookedDates: {
+                  bookingID1: {
+                    dates: { from: '30.03.2023', to: '01.04.2023' },
+                    userId: 'testUser',
+                  },
+
+                  bookingID2: {
+                    dates: { from: '03.04.2023', to: '05.04.2023' },
+                    userId: 'testUser',
+                  },
+                },
+              },
+            })
+          );
+        }
+      )
+    );
+    const thunk = makeBookingThunk({
+      ...bookingData,
+      userId: 'testUser',
+    });
+    await thunk(
+      dispatch,
+      () => {},
+      () => {}
+    );
+    const [start, end] = dispatch.mock.calls;
+    expect(start[0].type).toBe('booking/makeBooking/pending');
+    expect(start[0].payload).toBe(undefined);
+    expect(end[0].type).toBe('booking/makeBooking/fulfilled');
+    expect(end[0].payload).toMatchObject(bookingData);
+    expect(end[0].payload).toHaveProperty('bookingId');
+  });
+
+  it('booking failure - booking range is available partially', async () => {
+    server.use(
+      rest.get(
+        'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
+        (req, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({
+              1: {
+                bookedDates: {
+                  bookingID: {
+                    dates: { from: '01.04.2023', to: '02.04.2023' },
+                    userId: 'testUser',
+                  },
+                },
+              },
+            })
+          );
+        }
+      )
+    );
+    const thunk = makeBookingThunk({
+      ...bookingData,
+      userId: 'testUser',
+    });
+    await thunk(
+      dispatch,
+      () => {},
+      () => {}
+    );
+    const [start, end] = dispatch.mock.calls;
+    expect(start[0].type).toBe('booking/makeBooking/pending');
+    expect(start[0].payload).toBe(undefined);
+    expect(end[0].type).toBe('booking/makeBooking/rejected');
+    expect(end[0].payload).toBe(
+      'На данный период проживания комната уже забронирована'
+    );
+  });
+
+  it('booking failure - room already booked on the same dates', async () => {
+    server.use(
+      rest.get(
+        'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
+        (req, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({
+              1: {
+                bookedDates: {
+                  bookingID: {
+                    dates: { from: '01.04.2023', to: '03.04.2023' },
+                    userId: 'testUser',
+                  },
+                },
+              },
+            })
+          );
+        }
+      )
+    );
+    const thunk = makeBookingThunk({
+      ...bookingData,
+      userId: 'testUser',
+    });
+    await thunk(
+      dispatch,
+      () => {},
+      () => {}
+    );
+    const [start, end] = dispatch.mock.calls;
+    expect(start[0].type).toBe('booking/makeBooking/pending');
+    expect(start[0].payload).toBe(undefined);
+    expect(end[0].type).toBe('booking/makeBooking/rejected');
+    expect(end[0].payload).toBe(
+      'На данный период проживания комната уже забронирована'
+    );
+  });
+
+  it('booking failure - data is undefined', async () => {
+    server.use(
+      rest.post(
+        'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms/1/bookedDates.json',
+        (req, res, ctx) => {
+          return res(ctx.status(200));
+        }
+      )
+    );
+    const thunk = makeBookingThunk({
+      ...bookingData,
+      userId: 'testUser',
+    });
+    await thunk(
+      dispatch,
+      () => {},
+      () => {}
+    );
+    const [start, end] = dispatch.mock.calls;
+    expect(start[0].type).toBe('booking/makeBooking/pending');
+    expect(start[0].payload).toBe(undefined);
+    expect(end[0].type).toBe('booking/makeBooking/rejected');
+    expect(end[0].payload).toBe('Бронирование не подтверждено');
+  });
+
+  it('booking failure - unknown server error', async () => {
+    server.use(
+      rest.get(
+        'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
+        (req, res, ctx) => {
+          return res(ctx.status(404));
+        }
+      )
+    );
+    const thunk = makeBookingThunk({
+      ...bookingData,
+      userId: 'testUser',
+    });
+    await thunk(
+      dispatch,
+      () => {},
+      () => {}
+    );
+    const [start, end] = dispatch.mock.calls;
+    expect(start[0].type).toBe('booking/makeBooking/pending');
+    expect(start[0].payload).toBe(undefined);
+    expect(end[0].type).toBe('booking/makeBooking/rejected');
+    expect(end[0].payload).toBe('Request failed with status code 404');
+  });
 });
