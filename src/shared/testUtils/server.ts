1f39daf901331151e3c185c2f5e3cb26708d8c74
Ekaterina <1988snea@gmail.com>
2023-03-30 09:12:25 +0500

    testing: cases for like adding and removing

--- src/shared/testUtils/server.ts
+++ src/shared/testUtils/server.ts
@@ -3,6 +3,18 @@ import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 
 const server = setupServer(
+  rest.get(
+    'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          1: {},
+        })
+      );
+    }
+  ),
+
   rest.post(
     'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms/1/bookedDates.json',
     (req, res, ctx) => {
@@ -18,20 +30,16 @@ const server = setupServer(
   ),
 
   rest.post(
+    'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms/1/feedback/feedbackID/likes.json',
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json({}));
+    }
+  ),
+
+  rest.post(
     'https://react-village-d5bce-default-rtdb.firebaseio.com/users/testUser/booking.json',
     (req, res, ctx) => {
       return res(ctx.status(200));
-    }
-  ),
-  rest.get(
-    'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
-    (req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          1: {},
-        })
-      );
     }
   )
 );
--- src/store/slices/room/__tests__/slice.test.ts
+++ src/store/slices/room/__tests__/slice.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { rest } from 'msw';
@@ -5,6 +6,7 @@ import { rest } from 'msw';
 import { server } from '../../../../shared/testUtils/server';
 import {
   addFeedback as addFeedbackThunk,
+  changeLike as changeLikeThunk,
   fetchRoomById as fetchRoomThunk,
 } from '../slice';
 
@@ -14,8 +16,28 @@ afterEach(() => server.resetHandlers());
 
 const dispatch = jest.fn();
 
+const likeData = {
+  roomNumber: '2',
+  path: 'feedback/feedbackID/likes',
+  userId: 'testUser',
+};
+
+const feedbackPayload = {
+  text: 'feedback',
+  userId: 'testUser',
+  userName: 'Test User',
+  date: '2023-01-01T00:00:00.000Z',
+};
+
+const feedbackData = {
+  ...feedbackPayload,
+  roomNumber: '2',
+  path: 'feedback',
+  date: new Date('2023-01-01'),
+};
+
 describe('Room', () => {
-  it('successful room fetching', async () => {
+  it('Successful room fetching', async () => {
     server.use(
       rest.get(
         'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
@@ -44,7 +66,7 @@ describe('Room', () => {
     expect(end[0].payload).toMatchObject({ roomNumber: 2 });
   });
 
-  it('failed room fetching', async () => {
+  it('Failed room fetching', async () => {
     server.use(
       rest.get(
         'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
@@ -66,7 +88,7 @@ describe('Room', () => {
     expect(end[0].payload).toBe('Request failed with status code 404');
   });
 
-  it('successful feedback adding', async () => {
+  it('Successful feedback adding', async () => {
     const date = new Date('2023-01-01');
     server.use(
       rest.get(
@@ -79,9 +101,7 @@ describe('Room', () => {
                 roomNumber: 2,
                 feedback: {
                   feedbackID: {
-                    text: 'feedback',
-                    userId: 'testUser',
-                    userName: 'Test User',
+                    ...feedbackPayload,
                     date,
                   },
                 },
@@ -91,14 +111,7 @@ describe('Room', () => {
         }
       )
     );
-    const thunk = addFeedbackThunk({
-      roomNumber: '2',
-      path: 'feedback',
-      text: 'feedback',
-      userId: 'testUser',
-      userName: 'Test User',
-      date: new Date('2023-01-01'),
-    });
+    const thunk = addFeedbackThunk(feedbackData);
     await thunk(
       dispatch,
       () => {},
@@ -110,18 +123,11 @@ describe('Room', () => {
     expect(end[0].type).toBe('room/addFeedback/fulfilled');
     expect(end[0].payload).toMatchObject({
       roomNumber: 2,
-      feedback: {
-        feedbackID: {
-          text: 'feedback',
-          userId: 'testUser',
-          userName: 'Test User',
-          date: '2023-01-01T00:00:00.000Z',
-        },
-      },
+      feedback: { feedbackID: feedbackPayload },
     });
   });
 
-  it('failed feedback adding', async () => {
+  it('Failed feedback adding', async () => {
     server.use(
       rest.get(
         'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
@@ -130,14 +136,7 @@ describe('Room', () => {
         }
       )
     );
-    const thunk = addFeedbackThunk({
-      roomNumber: '2',
-      path: 'feedback',
-      text: 'feedback',
-      userId: 'testUser',
-      userName: 'Test User',
-      date: new Date('2023-01-01'),
-    });
+    const thunk = addFeedbackThunk(feedbackData);
     await thunk(
       dispatch,
       () => {},
@@ -147,6 +146,108 @@ describe('Room', () => {
     expect(start[0].type).toBe('room/addFeedback/pending');
     expect(start[0].payload).toBe(undefined);
     expect(end[0].type).toBe('room/addFeedback/rejected');
+    expect(end[0].payload).toBe('Request failed with status code 404');
+  });
+
+  it('Successful like adding', async () => {
+    server.use(
+      rest.get(
+        'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
+        (req, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({
+              1: {
+                roomNumber: 2,
+                feedback: {
+                  feedbackID: {
+                    ...feedbackPayload,
+                    likes: { likeID: 'testUser' },
+                  },
+                },
+              },
+            })
+          );
+        }
+      )
+    );
+    const thunk = changeLikeThunk({ ...likeData, isLiked: true });
+    await thunk(
+      dispatch,
+      () => {},
+      () => {}
+    );
+    const [start, end] = dispatch.mock.calls;
+    expect(start[0].type).toBe('room/changeLike/pending');
+    expect(start[0].payload).toBe(undefined);
+    expect(end[0].type).toBe('room/changeLike/fulfilled');
+    expect(end[0].payload).toMatchObject({
+      roomNumber: 2,
+      feedback: {
+        feedbackID: {
+          ...feedbackPayload,
+          likes: { likeID: 'testUser' },
+        },
+      },
+    });
+  });
+
+  it('Successful like removing', async () => {
+    server.use(
+      rest.get(
+        'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
+        (req, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({
+              1: {
+                roomNumber: 2,
+                feedback: {
+                  feedbackID: feedbackPayload,
+                },
+              },
+            })
+          );
+        }
+      )
+    );
+    const thunk = changeLikeThunk({ ...likeData, isLiked: false });
+    await thunk(
+      dispatch,
+      () => {},
+      () => {}
+    );
+    const [start, end] = dispatch.mock.calls;
+    expect(start[0].type).toBe('room/changeLike/pending');
+    expect(start[0].payload).toBe(undefined);
+    expect(end[0].type).toBe('room/changeLike/fulfilled');
+    expect(end[0].payload).toMatchObject({
+      roomNumber: 2,
+      feedback: {
+        feedbackID: feedbackPayload,
+      },
+    });
+  });
+
+  it('Failed like adding', async () => {
+    server.use(
+      rest.get(
+        'https://react-village-d5bce-default-rtdb.firebaseio.com/rooms.json',
+        (req, res, ctx) => {
+          return res(ctx.status(404));
+        }
+      )
+    );
+    const thunk = changeLikeThunk(likeData);
+    await thunk(
+      dispatch,
+      () => {},
+      () => {}
+    );
+    const [start, end] = dispatch.mock.calls;
+    expect(start[0].type).toBe('room/changeLike/pending');
+    expect(start[0].payload).toBe(undefined);
+    expect(end[0].type).toBe('room/changeLike/rejected');
     expect(end[0].payload).toBe('Request failed with status code 404');
   });
 });
